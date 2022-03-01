d1f1f86d7c42996170d7fe9c41ca4c90eb089cbc
joshcesana <joshuacesana@gmail.com>
2022-03-01 16:55:06 -0500

    Add directory-tree, index file, new readme

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # plantinfo-data-plants
-Plantinfo Data - Plants
+This repo contains the plants data for plantinfo.org.
+
+*To create an index file:*
+
+Run
+
+```sh
+   npm install
+```
+
+To create **file-index.json**, run:
+
+```javascript
+   node index
+```
+
+In `index.js`, settings available to edit:
+
+```javascript
+const fileName = 'file-index.json'
+const minified = true
+```
---
+++ index.js
@@ -0,0 +1,15 @@
+const dirTree = require('directory-tree')
+const fs = require('fs')
+const fileName = 'file-index.json'
+const minified = true
+
+function dirCallback (item) {
+  delete item.path // remove 'path' key from all directory objects
+}
+
+const tree = dirTree('plants', { attributes: ['type'] }, null, dirCallback)
+
+fs.appendFile(fileName, JSON.stringify(tree, null, minified ? 0 : 2), function (err) {
+  if (err) throw err
+  console.log(`Saved to ${fileName}`)
+})
